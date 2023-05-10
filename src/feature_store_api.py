8a3617a3ccaf25f523ec8d52866dab5d842e97da
Santiago Lopez <santiagolopez@sqdm.com>
2023-05-10 11:26:28 -0500

    Change feature pipeline

---
+++ src/feature_store_api.py
@@ -0,0 +1,19 @@
+from typing import Optional
+import hsfs
+import hopsworks
+
+import src.config as config
+
+def get_feature_store() -> hsfs.feature_store.FeatureStore:
+    """Connects to Hopsworks and returns a pointer to the feature store
+
+    Returns:
+        hsfs.feature_store.FeatureStore: pointer to the feature store
+    """
+    project = hopsworks.login(
+        project=config.HOPSWORKS_PROJECT_NAME,
+        api_key_value=config.HOPSWORKS_API_KEY
+    )
+    return project.get_feature_store()
+
+
